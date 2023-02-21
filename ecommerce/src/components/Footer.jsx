bec97c8a74c1113c6fedda0f70a88bf82e76c57f
jamesdosstech <jamesdosstech@gmail.com>
2023-02-20 18:02:27 -0600

    added backend

---
+++ ecommerce/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Container, Col, Row } from "react-bootstrap";
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center py-3">Copywrite &copy; ProShop</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
